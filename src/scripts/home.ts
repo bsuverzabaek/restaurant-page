2721f9fef12cf7a307ce0ca1a87799aa981bc678
bsuverzabaek <69542867+bsuverzabaek@users.noreply.github.com>
2021-08-23 00:01:22 -0600

    7th commit

--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -1,7 +1,8 @@
 import { defineComponent } from 'vue';
 import Imgslides from '@/components/Imgslides.vue';
+import { BIconStarFill } from 'bootstrap-icons-vue';
 
 export default defineComponent({
 	name: 'Home',
-    components: { Imgslides }
+    components: { Imgslides, BIconStarFill }
 });
--- src/views/EN/Home.vue
+++ src/views/EN/Home.vue
@@ -60,6 +60,97 @@
 			</div>
 		</div>
 	</section>
+
+	<section>
+		<div class="text-center">
+			<div class="fs-2 text-decoration-underline">
+				Customer Reviews
+			</div>
+			<div class="container-lg">
+				<div class="row justify-content-center my-3">
+					<div class="col-lg-8">
+						<div class="list-group">
+							<div class="list-group-item py-3">
+								<div class="pb-2">
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+								</div>
+								<p class="mb-3">
+									"We've been one of the first customers to come to this establishment since it opened and have continued coming here ever since. This is a great place for families, couples, etc. I would recommend this restaurant to everyone."
+								</p>
+								<figcaption class="blockquote-footer">
+									Maria S.
+								</figcaption>
+							</div>
+							<div class="list-group-item py-3">
+								<div class="pb-2">
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+								</div>
+								<p class="mb-3">
+									"My wife and I just happened to find this place, and we're really glad we did. The food, the staff, and the atmosphere provide a very nice experience. We'll be visiting this place often from now on."
+								</p>
+								<figcaption class="blockquote-footer">
+									Thomas D.
+								</figcaption>
+							</div>
+							<div class="list-group-item py-3">
+								<div class="pb-2">
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+								</div>
+								<p class="mb-3">
+									"As a self-proclaimed Italian cuisine connoisseur, I was a bit skeptical when I read the reviews for this restaurant. I will admit for the first time in a long time that I was mistaken. I have enjoyed the meals very much and highly recommend this place to everyone."
+								</p>
+								<figcaption class="blockquote-footer">
+									Steven G.
+								</figcaption>
+							</div>
+							<div class="list-group-item py-3">
+								<div class="pb-2">
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+								</div>
+								<p class="mb-3">
+									"My husband and I have tried almost every single dish offered in this restaurant. We have yet to be disappointed!"
+								</p>
+								<figcaption class="blockquote-footer">
+									Cassie K.
+								</figcaption>
+							</div>
+							<div class="list-group-item py-3">
+								<div class="pb-2">
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+									<BIconStarFill />
+								</div>
+								<p class="mb-3">
+									"The kids constantly ask to eat here every week. I'm not complaining though. I just wish we lived closer."
+								</p>
+								<figcaption class="blockquote-footer">
+									Martin T.
+								</figcaption>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script lang="ts" src="@/scripts/home.ts">
