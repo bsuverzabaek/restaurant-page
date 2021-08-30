c2f177870e38228951df87e1e038db918c9a386f
bsuverzabaek <69542867+bsuverzabaek@users.noreply.github.com>
2021-08-30 00:08:37 -0600

    13th commit

---
+++ src/interfaces/menuStruct.ts
@@ -0,0 +1,8 @@
+interface Menu {
+	id: number,
+	name: string,
+	price: number,
+	desc: string
+}
+
+export default Menu
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,5 +1,27 @@
 <template>
 	<h1>{{ jSwitch ? 'メニュー' : 'Menu' }}</h1>
+
+	<section id="appetizer">
+		<div class="text-center">
+			<div 
+				class="fs-2 text-decoration-underline"
+			>
+				{{ jSwitch ? '前菜' : 'Appetizers' }}
+			</div>
+
+			<div class="container-lg my-3">
+				<div class="row justify-content-center gx-3">
+					<div class="col-md-6">
+						<span>Salad</span>
+						<span>$5.99</span>
+					</div>
+					<div class="col-md-6">
+						<div>sdfdsfds</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script lang="ts" src="@/scripts/menu.ts">
